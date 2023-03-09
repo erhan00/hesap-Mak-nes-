4f67efdf2b3cda1bec88840a13fa46f025de3ec1
erhan <blsmkodlari00@gmail.com>
2023-03-09 14:38:18 +0300

    javascript guncellendı ve mobil

--- hesap.js
+++ hesap.js
@@ -1,6 +1,33 @@
 
+//    ******** Birazdaha işlevsellik üzerine çalişilacak   *********** 
 
- 
+const düğme = document.getElementsByTagName("button");
+//console.log(düğme);
+const ekrann = document.getElementById("ekran2");
 
-  
+const temizlee = document.getElementById("temizle");
 
+const hesap = document.getElementById("hesapla");
+
+for(let i =0; i<düğme.length; i++){
+    if(i != 7 && i != 11 && i != 15 && i != 18)
+    {
+      düğme[i].addEventListener("click",yazdir)
+    }
+    }
+
+function yazdir(){
+    ekrann.value = ekrann.value + this.value
+}
+
+temizlee.addEventListener("click",temiizle)
+
+function temiizle(){
+    ekrann.value=" ";
+}
+
+hesap.addEventListener("click",hesapp)
+
+function hesapp(){
+    ekrann.value = eval(ekrann.value);
+}
